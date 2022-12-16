e15cadc3b9fc36d4ed9d2e90cafe7dacb5a38dc9
Alper Karabıyık <120407040+karabiyikk@users.noreply.github.com>
2022-12-17 02:09:07 +0300

    Add files via upload

---
+++ UsAlma.java
@@ -0,0 +1,20 @@
+package Donguler;
+
+import java.util.Scanner;
+
+public class UsAlma {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int k, n, i, toplam = 1;
+
+        System.out.print("Üssü alıınacak sayıyı girin : ");
+        k = input.nextInt();
+        System.out.print("Üs kuvvetini girin : ");
+        n = input.nextInt();
+
+        for (i = 1; i <= n; i++) {
+            toplam *= k;
+        }
+        System.out.println(toplam);
+    }
+}
